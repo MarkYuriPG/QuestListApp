742a41807db3738758de7571d3ee30f3c5e56598
Mark Yuri Geagonia <markyuri.geagonia@cit.edu>
2023-04-04 20:47:24 +0800

    Implement hide action bar

--- app/src/main/java/com/example/questlistapp/SplashActivity.java
+++ app/src/main/java/com/example/questlistapp/SplashActivity.java
@@ -9,6 +9,9 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_splash);
     }
 }
