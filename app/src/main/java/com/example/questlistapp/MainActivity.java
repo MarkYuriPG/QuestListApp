de8a044e85dadd3740e0cb0ef9f52e9dfd1a90bc
Mark Yuri Geagonia <markyuri.geagonia@cit.edu>
2023-04-05 11:24:52 +0800

    Declare questRecyclerView

--- app/src/main/java/com/example/questlistapp/MainActivity.java
+++ app/src/main/java/com/example/questlistapp/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.questlistapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView questRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
 
-        setContentView(R.layout.activity_main);
+        questRecyclerView = findViewById(R.id.questRecyclerView);
+        questRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
