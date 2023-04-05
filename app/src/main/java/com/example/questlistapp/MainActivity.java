545a5534c1ae73739e4df8fdc6a857da2f45be73
Mark Yuri Geagonia <markyuri.geagonia@cit.edu>
2023-04-05 12:58:04 +0800

    Add test quest list on recyclerview

--- app/src/main/java/com/example/questlistapp/MainActivity.java
+++ app/src/main/java/com/example/questlistapp/MainActivity.java
@@ -6,10 +6,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.questlistapp.Adapter.ToDoAdapter;
+import com.example.questlistapp.Model.ToDoModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView questRecyclerView;
+    private ToDoAdapter questAdapter;
 
+    private List<ToDoModel> questList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +25,25 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
+        questList = new ArrayList<>();
+
         questRecyclerView = findViewById(R.id.questRecyclerView);
         questRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        questAdapter = new ToDoAdapter(this);
+        questRecyclerView.setAdapter(questAdapter);
+
+        ToDoModel quest = new ToDoModel();
+        quest.setQuest("This is a Quest!!!");
+        quest.setStatus(0);
+        quest.setId(1);
+
+        questList.add(quest);
+        questList.add(quest);
+        questList.add(quest);
+        questList.add(quest);
+        questList.add(quest);
+
+        questAdapter.setQuest(questList);
+
     }
 }
