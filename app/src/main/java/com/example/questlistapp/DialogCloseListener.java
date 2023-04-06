d7d21b362b0baa59b63c4359b21efde907c9de34
Mark Yuri Geagonia <markyuri.geagonia@cit.edu>
2023-04-06 23:19:22 +0800

    Erase extra handledialogclose

--- app/src/main/java/com/example/questlistapp/DialogCloseListener.java
+++ app/src/main/java/com/example/questlistapp/DialogCloseListener.java
@@ -3,6 +3,5 @@ package com.example.questlistapp;
 import android.content.DialogInterface;
 
 public interface DialogCloseListener {
-    public void handleDialogClsoe(DialogInterface dialog);
-    void handleDialogClose(DialogInterface dialog);
+    public void handleDialogClose(DialogInterface dialog);
 }
